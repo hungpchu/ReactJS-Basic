9cbb97aedfc6824e0f4c09d5011f3c136e947665
HUNG CHU <cphung1997@gmail.com>
2020-06-03 17:58:30 -0500

    Add the ReactMySQLProject with no database with just client and server

---
+++ ReactMySQLProject/README.md
@@ -0,0 +1,12 @@
+run app
+1/ `npm run dev` -> chạy kết hợp giữa server và client bằng lệnh này.
+2/ `npm run client`-> client chạy bằng lệnh này.
+3/ `npm run server` -> server chạy bằng lệnh này.
+client: react
+server: express
+Note: I do not add node_modules since it is so big so run this command in your directory
+0/ `cd ReactMySQLProject`
+1/ `npm install –save express -> install node-modules`
+2/ `npm install –save concurrently`
+3/ `npm install –save axios`
+
---
+++ ReactMySQLProject/client/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+  state = {
+    message: ''
+  };
+
+  componentDidMount() {
+    axios.get('/api/test')
+         .then(result => this.setState({ message: result.data.message }))
+  };
+
+  render() {
+    return(
+      <div className="App">
+        <header className="App-header">
+          <h1>{ this.state.message }</h1>
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+    )
+  };
+};
+
+export default App;
